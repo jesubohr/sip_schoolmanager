497f113180895bcda30f346b7ca231009db5f505
Jesus Borrero <jesufer2018@hotmail.com>
2024-03-22 01:07:37 -0500

    Add types for input fields and form related types

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export interface InputField {
+  type: string
+  id: string
+  name: string
+  label?: string
+  value?: string
+  placeholder?: string
+  required: boolean
+  validators: InputValidator[]
+}
+export type InputValidator = (value: string) => {
+  isValid: boolean
+  message: string
+}
+export type SubmitAction = (...args: any[]) => void
+export type Method = "GET" | "POST" | "PUT" | "DELETE"
