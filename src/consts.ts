6e00b437e6469d0b48a946f216850127b4692b9e
Jesus Borrero <jesufer2018@hotmail.com>
2024-03-23 17:00:35 -0500

    chore(app): Add SERVER_BASE_URL constant

---
+++ src/consts.ts
@@ -0,0 +1,1 @@
+export const SERVER_BASE_URL = import.meta.env.SERVER_BASE_URL ?? "http://localhost:3000"
---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  readonly SERVER_BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
