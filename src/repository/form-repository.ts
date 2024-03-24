e0a231f9987a86549e7ad8257f9a6216ab50e2ae
Jesus Borrero <jesufer2018@hotmail.com>
2024-03-23 19:28:29 -0500

    refactor(app): Update type of formData in sendData method

--- src/repository/form-repository.ts
+++ src/repository/form-repository.ts
@@ -3,7 +3,7 @@ import { SERVER_BASE_URL } from "@/consts"
 export class FormRepository {
   constructor() {}
 
-  async sendData(endpoint: string, formData: Record<string, string>) {
+  async sendData(endpoint: string, formData: Record<string, unknown>) {
     try {
       const response = await fetch(`${SERVER_BASE_URL}/${endpoint}`, {
         mode: "no-cors",
@@ -13,7 +13,7 @@ export class FormRepository {
           "Content-Type": "application/json"
         }
       })
-      if (!response.ok) throw new Error("There was an error with the request")
+      if (!response.ok) throw new Error("There was an error with the request. Please try again.")
 
       const data = await response.json()
       return { success: data.success, message: data.message }
