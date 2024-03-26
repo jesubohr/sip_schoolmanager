7278645c07f507e0ab865761fca256002f87c94f
Jesus Borrero <jesufer2018@hotmail.com>
2024-03-26 00:53:00 -0500

    chore(app): Update SubmitAction type to accept generic arguments

--- src/types.ts
+++ src/types.ts
@@ -12,5 +12,5 @@ export type InputValidator = (value: string) => {
   isValid: boolean
   message: string
 }
-export type SubmitAction = (...args: unknown[]) => void
+export type SubmitAction<T> = (...args: T[]) => void
 export type Method = "GET" | "POST" | "PUT" | "DELETE"
