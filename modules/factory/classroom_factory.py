46068a5b32638bc7e561b8f98e724c75b4688870
Jesus Borrero <jesufer2018@hotmail.com>
2024-03-26 00:53:00 -0500

    feat(app): Add ClassroomFactory class to create Classroom instances

---
+++ modules/factory/classroom_factory.py
@@ -0,0 +1,19 @@
+from modules.singleton import Singleton
+from modules.models.classroom import Classroom
+
+class ClassromFactory(metaclass = Singleton):
+  def __init__(self):
+    self._cache = {}
+
+  def create_classroom(self, data: dict) -> Classroom:
+    ''' Create new Classroom instance or returns existing one '''
+
+    classroom_id = data['id']
+    if classroom_id not in self._cache:
+      classroom = Classroom(
+        name = data['name'],
+        description = data['description'],
+        representative = data['representative']
+      )
+      self._cache[classroom_id] = classroom
+    return self._cache[classroom_id]
