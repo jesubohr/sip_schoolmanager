65c6e3c55bf6498fa884b4abef9fe53054f98b92
Jesus Borrero <jesufer2018@hotmail.com>
2024-03-26 00:53:00 -0500

    feat(app): Add Singleton metaclass implementation

---
+++ modules/singleton.py
@@ -0,0 +1,15 @@
+from threading import Lock
+
+class Singleton(type):
+  ''' Metaclass for singleton classes '''
+  ''' Based on https://bit.ly/3xivrif '''
+
+  _instances = {}
+  _lock: Lock = Lock()
+
+  def __call__(cls, *args, **kwargs):
+    with cls._lock:
+      if cls not in cls._instances:
+        instance = super(Singleton, cls).__call__(*args, **kwargs)
+        cls._instances[cls] = instance
+    return cls._instances[cls]
