eeb0ff0164ec815824662fa714f0e70d066f46f7
Jesus Borrero <jesufer2018@hotmail.com>
2024-03-31 01:18:10 -0500

    feat(app): Add headers and method to fetch request

--- src/utils/classrooms.ts
+++ src/utils/classrooms.ts
@@ -2,7 +2,13 @@ import { SERVER_BASE_URL } from "@/consts"
 
 export async function getClassroomOptions() {
   try {
-    const response = await fetch(`${SERVER_BASE_URL}/schoolmanager/classrooms/get_all`)
+    const response = await fetch(`${SERVER_BASE_URL}/schoolmanager/classrooms/get_all`, {
+      mode: "no-cors",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
     if (!response.ok) throw new Error("Failed to fetch classrooms")
     const data = await response.json()
     return data.map((classroom: { id: number, name: string }) => ({
