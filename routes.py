aa3741d17768ca6cd45715f8357a32069dc25b6b
Jesus Borrero <jesufer2018@hotmail.com>
2024-03-31 01:16:25 -0500

    feat(app): Add functions for schoolmanager other controllers

--- routes.py
+++ routes.py
@@ -4,6 +4,13 @@ routers = dict(
   ),
   schoolmanager = dict(
     default_controller='default',
-    default_function='index'
+    default_function='index',
+    functions = dict(
+      default=['index'],
+      students=['index', 'add', 'create'],
+      subjects=['index'],
+      classrooms=['index', 'get_all', 'create'],
+      attendances=['index', 'update_attendance']    
+    )
   ),
 )
